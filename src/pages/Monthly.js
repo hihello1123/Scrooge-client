e43c8a4ded0aeaa2b0b01147537dfec75dc676d1
hihello1123 <hihello1123@naver.com>
2021-07-07 10:21:02 +0900

    Feat monthly

--- src/pages/Monthly.js
+++ src/pages/Monthly.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Topper from '../components/Topper';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
 function Monthly() {
   return (
     <div className="container">
       <Topper />
       <div>월별 하단 라이브러리 예정</div>
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
     </div>
   );
 }
